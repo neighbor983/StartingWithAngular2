1afa20d7259554d0e05d5192a327edc14610fe14
neighbor983 <kenneth.ray.baker@gmail.com>
2017-02-11 05:51:23 +0000

    completed lesson 23

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from 'angular2/core';
+import { EventListComponent } from './events/event-list.component';
 
 @Component({
     selector: 'events-app',
     template: `
         <div>
             <h1>{{pageTitle}}</h1>
-            <div>Super Simple Component</div>
+            <el-events></el-events>
         </div>
-    `
+    `,
+    directives: [EventListComponent]
 })
 
 export class AppComponent {
