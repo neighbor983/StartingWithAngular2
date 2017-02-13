b89a33bc6792d829b0030651c1a835d9cc9edf76
neighbor983 <kenneth.ray.baker@gmail.com>
2017-02-13 05:58:22 +0000

    completed lesson 58

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from 'angular2/core';
 import { EventListComponent } from './events/event-list.component';
+import { WelcomeComponent } from './home/welcome.component';
 import { EventService } from './events/event.service';
 import { HTTP_PROVIDERS } from 'angular2/http';
 import 'rxjs/Rx';
+import { ROUTER_PROVIDERS, RouteConfig } from 'angular2/router';
 
 @Component({
     selector: 'events-app',
@@ -12,9 +14,14 @@ import 'rxjs/Rx';
             <el-events></el-events>
         </div>
     `,
-    directives: [EventListComponent],
-    providers: [EventService, HTTP_PROVIDERS]
+    directives: [EventListComponent, WelcomeComponent],
+    providers: [EventService, HTTP_PROVIDERS, ROUTER_PROVIDERS]
 })
+
+@RouteConfig([
+    { path: '/welcome', name: 'Welcome', component: WelcomeComponent, useAsDefault: true },
+    { path: '/events', name: 'Events', component: EventListComponent }
+])
 
 export class AppComponent {
     pageTitle: string = 'Local Events App';
